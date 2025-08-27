7d1949367fced7e7eba8f614f815c205f757d48a
Augusto Peres <augustoapg@gmail.com>
2025-08-27 14:32:49 -0400

    improve json output readability in dev environments

--- internal/utils/http_utils.go
+++ internal/utils/http_utils.go
@@ -3,12 +3,22 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 )
 
 func SendJSONResponse(w http.ResponseWriter, statusCode int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	json.NewEncoder(w).Encode(data)
+
+	if os.Getenv("ENV") != "production" {
+		// prettify JSON for easier debugging
+		encoder := json.NewEncoder(w)
+		encoder.SetIndent("", "  ")
+		encoder.Encode(data)
+	} else {
+		// in production, compact JSON for smaller payload
+		json.NewEncoder(w).Encode(data)
+	}
 }
 
 func SendErrorResponse(w http.ResponseWriter, statusCode int, message string) {
